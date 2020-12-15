4c8c2cce9557ae0f4e9bf498b0bc31ebf7b10a7b
Mythbusters123 <58893124+Mythbusters123@users.noreply.github.com>
2020-12-15 15:18:11 -0500

    Fixed ping command being a weird boi

--- src/Commands/Utilities/Ping.js
+++ src/Commands/Utilities/Ping.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
         const msg = await message.channel.send('Pinging...');
 
         const latency = msg.createdTimestamp - message.createdTimestamp;
-        const choices = [`Is it really my ping?', 'Is this ok? I can\'t look!', 'I hope it isn\'t bad!`];
+        const choices = ['Is it really my ping?', 'Is this ok? I can\'t look!', 'I hope it isn\'t bad!'];
         const response = choices[Math.floor(Math.random() * choices.length)];
 
         msg.edit(`${response} - Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
