b4cd169bc57c3515183b09a7f967dde4efc16fc1
Mythbusters123 <58893124+Mythbusters123@users.noreply.github.com>
2020-12-15 15:23:30 -0500

    made README.md a thing lol

---
+++ README.md
@@ -0,0 +1,2 @@
+#IQ Bot
+If you are seeing this and are not: Prithibi, BananasAmIRite, or Mythbusters123; back off!
--- src/Commands/CheckXP.js
+++ src/Commands/CheckXP.js
@@ -28,17 +28,17 @@ module.exports = class extends command {
 			switch (member.rank) {
 				case '1st Quad':
 					if(total < 75000 && member.joined < ((new Date()).getUTCMilliseconds() - (7*24*60*60*1000))){
-						naughtyPlayers.set(mcapi.nameForUuid(member.uuid), total)
+						naughtyPlayers.set(`${await mcapi.nameForUuid(member.uuid)} (1st Quad)`, total)
 					}
 					break;
 				case '2nd Quad':
 					if(total < 50000 && member.joined < (new Date().getUTCMilliseconds() - (7*24*60*60*1000))){
-						naughtyPlayers.set(`${await mcapi.nameForUuid(member.uuid)} - `, total)
+						naughtyPlayers.set(`${await mcapi.nameForUuid(member.uuid)} (2nd Quad)`, total)
 					}
 					break;
 				case 'Staff':
 					if(total < 30000){
-						naughtyPlayers.set(mcapi.nameForUuid(member.uuid), total)
+						naughtyPlayers.set(`${await mcapi.nameForUuid(member.uuid)} (2nd Quad)`, total)
 					}
 					break;
 				default:
@@ -51,8 +51,9 @@ module.exports = class extends command {
 }
 function buildMessage(map, message){
 	let description = '';
+	let i = 1;
 	for(const [key, value] of map){
-
+		description = description + `${i}. ${key} - ${value} GEXP. \n`
 	}
 }
 
