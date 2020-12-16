3e98e8e10c20953e003516a25e66461ade149141
Mythbusters123 <58893124+Mythbusters123@users.noreply.github.com>
2020-12-16 01:28:22 -0500

    Update index.js
    added todo so i wont forget

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ let joinMessages = [
     'Welcome, %n. We were expecting you ;)', 'Welcome, %n. We hope you brought pizza.', 'Welcome %n. Leave your weapons by the door.',
     'A wild %n appeared.', 'Swoooosh. %n just landed.', 'Brace yourselves. %n just joined the guild.', '%n just joined. Hide your bananas.',
     '%n just arrived. Seems OP - please nerf.', '%n just slid into the guild.', 'A %n has spawned in the server.', 'Big %n showed up!',
-    'Where’s %n? In the guild!',
+    'Where’s %n? In the guild!', //TODO: FINISH LIST
 ]
 
 //Join message
